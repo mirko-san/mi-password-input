b85056229487f2df12f17382d11cdee5d9ff7f1c
mirko-san <41379069+mirko-san@users.noreply.github.com>
2024-03-16 16:09:30 +0900

    :sparkles: Build option

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,20 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/password-input.ts',
+      name: 'password-input',
+      fileName: (format) => `password-input.${format}.js`,
+    },
+    rollupOptions: {
+      input: 'src/password-input.ts',
+      output: {
+        dir: 'dist/',
+        format: 'es',
+      },
+    },
+  },
   server: {
     port: 3080,
   },
